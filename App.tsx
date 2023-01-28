6d2d7bbfaf63e6f8b99bffefe9441645cb055ff2
Flávio Dutra <flaviomdutra@outlook.com>
2023-01-28 19:59:42 -0300

    feat: prebuild

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId("9fedd404-002c-4d09-9b72-650146051dcf");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
