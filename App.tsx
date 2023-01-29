9623b4121f3a8d1142a49c7b214e477409ee3f6a
Flávio Dutra <flaviomdutra@outlook.com>
2023-01-29 00:07:37 -0300

    feat: add notification on OneSignal

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
-import { StatusBar } from 'react-native';
-import OneSignal from 'react-native-onesignal';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Platform, StatusBar } from "react-native";
+import OneSignal from "react-native-onesignal";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+import { tagUserInfoCreate } from "./src/notifications/notificationsTags";
 
-OneSignal.setAppId("9fedd404-002c-4d09-9b72-650146051dcf");
+const oneSignalAppId =
+  Platform.OS === "ios"
+    ? "9fedd404-002c-4d09-9b72-650146051dcf"
+    : "9fedd404-002c-4d09-9b72-650146051dcf";
+OneSignal.setAppId(oneSignalAppId);
+
+OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+  console.log(response);
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserInfoCreate()
 
   return (
     <NativeBaseProvider theme={THEME}>
